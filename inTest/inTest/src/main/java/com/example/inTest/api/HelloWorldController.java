bce5b74e2f728b28bd5f04353d563c37465866e1
Лев Шинкоренко <Lev Shinkorenko>
2023-07-11 11:12:59 +0300

    Add hello world

---
+++ inTest/inTest/src/main/java/com/example/inTest/api/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.example.inTest.api;
+
+import lombok.Data;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Data
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("hello-world")
+    public String HelloWorld(){
+        return "HelloWorld";
+    }
+}
