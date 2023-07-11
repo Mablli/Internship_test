6e07fede6c742389c07c34ec2335bbe6b9ba6ea7
Лев Шинкоренко <Lev Shinkorenko>
2023-07-11 09:41:53 +0300

    Lvl 1 completed

---
+++ inTest/inTest/src/main/java/com/example/inTest/InTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.inTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InTestApplication.class, args);
+	}
+
+}
---
+++ inTest/inTest/src/test/java/com/example/inTest/InTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
